843b69b312c059eda35bd1fa07dd26afaaa18797
Srikanth Palakuri <palakuri.srikanth23@gmail.com>
2024-04-28 23:56:11 +0530

    adding comma for the separation

--- modules/app/main.tf
+++ modules/app/main.tf
@@ -22,7 +22,7 @@ resource "null_resource" "ansible" {
 
     inline = [
       "sudo pip3.11 install ansible hvac",
-      "ansible-pull -i localhost, -U https://github.com/Srikanth-Git123/expense-ansible get-secrets.yml -e env=${var.env} -e role_name=${var.component} -e vault_token=${var.vault_token}"
+      "ansible-pull -i localhost, -U https://github.com/Srikanth-Git123/expense-ansible get-secrets.yml -e env=${var.env} -e role_name=${var.component} -e vault_token=${var.vault_token}",
       "ansible-pull -i localhost, -U https://github.com/Srikanth-Git123/expense-ansible expense.yml -e env=${var.env} -e role_name=${var.component} -e @secrets.json @app.json)"
     ]
   }
