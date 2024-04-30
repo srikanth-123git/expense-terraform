45b79fd43d4689094475e5272902dcaa95589c83
Srikanth Palakuri <palakuri.srikanth23@gmail.com>
2024-04-30 16:57:21 +0530

    Creating VPC.

--- main.tf
+++ main.tf
@@ -1,30 +1,35 @@
-module "frontend" {
-  depends_on = [module.backend]
+# module "frontend" {
+#   depends_on = [module.backend]
+#
+#   source        = "./modules/app"
+#   instance_type = var.instance_type
+#   component     = "frontend"
+#   env           = var.env
+#   zone_id       = var.zone_id
+#   vault_token   = var.vault_token
+# }
+#
+# module "backend" {
+#   depends_on = [module.mysql]
+#
+#   source        = "./modules/app"
+#   instance_type = var.instance_type
+#   component     = "backend"
+#   env           = var.env
+#   zone_id       = var.zone_id
+#   vault_token   = var.vault_token
+# }
+#
+# module "mysql" {
+#   source        = "./modules/app"
+#   instance_type = var.instance_type
+#   component     = "mysql"
+#   env           = var.env
+#   zone_id       = var.zone_id
+#   vault_token   = var.vault_token
+# }
 
-  source        = "./modules/app"
-  instance_type = var.instance_type
-  component     = "frontend"
-  env           = var.env
-  zone_id       = var.zone_id
-  vault_token   = var.vault_token
-}
-
-module "backend" {
-  depends_on = [module.mysql]
-
-  source        = "./modules/app"
-  instance_type = var.instance_type
-  component     = "backend"
-  env           = var.env
-  zone_id       = var.zone_id
-  vault_token   = var.vault_token
-}
-
-module "mysql" {
-  source        = "./modules/app"
-  instance_type = var.instance_type
-  component     = "mysql"
-  env           = var.env
-  zone_id       = var.zone_id
-  vault_token   = var.vault_token
+module "vpc" {
+  source = "./modules/vpc"
+  env    = var.env
 }
