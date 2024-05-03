8156c33cf3e2de545725cfe8b1a15de9c906c61a
Srikanth Palakuri <palakuri.srikanth23@gmail.com>
2024-05-03 16:05:22 +0530

    Update DNS record

--- modules/app/main.tf
+++ modules/app/main.tf
@@ -66,11 +66,21 @@ resource "null_resource" "ansible" {
   }
 }
 
-resource "aws_route53_record" "record" {
+resource "aws_route53_record" "server" {
+  count          = var.lb_needed ? 0 : 1
   name           = "${var.component}-${var.env}"
   type           = "A"
   zone_id        = var.zone_id
   records        = [aws_instance.instance.private_ip]
+  ttl            = 30
+}
+
+resource "aws_route53_record" "load-balancer" {
+  count          = var.lb_needed ? 1 : 0
+  name           = "${var.component}-${var.env}"
+  type           = "CNAME"
+  zone_id        = var.zone_id
+  records        = [aws_lb.main[0].dns_name]
   ttl            = 30
 }
 
