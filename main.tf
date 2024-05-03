df824fda884b154dc0da4ea098a5ea5ab9255d69
Srikanth Palakuri <palakuri.srikanth23@gmail.com>
2024-05-03 16:49:13 +0530

    changing security groups and allowing limited access.

--- main.tf
+++ main.tf
@@ -15,7 +15,7 @@ module "frontend" {
   app_port                = 80
   bastion_nodes           = var.bastion_nodes
   prometheus_nodes        = var.prometheus_nodes
-  server_app_port_sg_cidr = module.vpc.public_subnets
+  server_app_port_sg_cidr = var.public_subnets
   lb_app_port_sg_cidr     = ["0.0.0.0/0"]
 }
 
@@ -36,8 +36,8 @@ module "backend" {
   app_port                = 8080
   bastion_nodes           = var.bastion_nodes
   prometheus_nodes        = var.prometheus_nodes
-  server_app_port_sg_cidr = concat(module.vpc.frontend_subnets, module.vpc.backend_subnets)
-  lb_app_port_sg_cidr     = module.vpc.frontend_subnets
+  server_app_port_sg_cidr = concat(var.frontend_subnets, var.backend_subnets)
+  lb_app_port_sg_cidr     = var.frontend_subnets
 }
 
 module "mysql" {
@@ -52,7 +52,7 @@ module "mysql" {
   bastion_nodes           = var.bastion_nodes
   prometheus_nodes        = var.prometheus_nodes
   app_port                = 3306
-  server_app_port_sg_cidr = module.vpc.backend_subnets
+  server_app_port_sg_cidr = var.backend_subnets
 }
 
 module "vpc" {
