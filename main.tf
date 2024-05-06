04593adfc920c9d513c6a56062ea2508ad7bc276
Srikanth Palakuri <palakuri.srikanth23@gmail.com>
2024-05-07 02:36:36 +0530

    Adding Auto scalling group to frontend.

--- main.tf
+++ main.tf
@@ -22,28 +22,22 @@
 #   kms_key_id              = var.kms_key_id
 # }
 #
-# module "backend" {
-#   depends_on = [module.rds]
-#
-#   source                  = "./modules/app"
-#   instance_type           = var.instance_type
-#   component               = "backend"
-#   env                     = var.env
-#   zone_id                 = var.zone_id
-#   vault_token             = var.vault_token
-#   subnets                 = module.vpc.backend_subnets
-#   vpc_id                  = module.vpc.vpc_id
-#   lb_type                 = "private"
-#   lb_needed               = true
-#   lb_subnets              = module.vpc.backend_subnets
-#   app_port                = 8080
-#   bastion_nodes           = var.bastion_nodes
-#   prometheus_nodes        = var.prometheus_nodes
-#   server_app_port_sg_cidr = concat(var.frontend_subnets, var.backend_subnets)
-#   lb_app_port_sg_cidr     = var.frontend_subnets
-#   lb_ports                = {http: 8080}
-#   kms_key_id              = var.kms_key_id
-# }
+
+module "frontend" {
+  source                  = "./modules/app-asg"
+  app_port                = 80
+  bastion_nodes           = var.bastion_nodes
+  component               = "frontend"
+  env                     = var.env
+  instance_type           = var.instance_type
+  max_capacity            = var.max_capacity
+  min_capacity            = var.min_capacity
+  prometheus_nodes        = var.prometheus_nodes
+  server_app_port_sg_cidr = var.public_subnets
+  subnets                 = module.vpc.frontend_subnets
+  vpc_id                  = module.vpc.vpc_id
+  vault_token             = var.vault_token
+}
 
 module "backend" {
   source                  = "./modules/app-asg"
