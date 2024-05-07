180f55c21e8f72eb6b64517f22d9e1aeb8d31dec
Srikanth Palakuri <palakuri.srikanth23@gmail.com>
2024-05-08 03:28:27 +0530

    Using golden AMI for app setup

--- modules/app-asg/data.tf
+++ modules/app-asg/data.tf
@@ -1,7 +1,7 @@
 data "aws_ami" "ami" {
   most_recent = true
-  name_regex  = "RHEL-9-DevOps-Practice"
-  owners      = ["973714476881"]
+  name_regex  = "golden-ami-*"
+  owners      = ["self"]
 }
 
 
--- modules/app-asg/userdata.sh
+++ modules/app-asg/userdata.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-pip3.11 install ansible hvac &>>/opt/ansible.log
 ansible-pull -i localhost, -U https://github.com/psrikanth/expense-ansible get-secrets.yml -e env=${env} -e role_name=${component}  -e vault_token=${vault_token} &>>/opt/ansible.log
 ansible-pull -i localhost, -U https://github.com/psrikanth/expense-ansible expense.yml -e env=${env} -e role_name=${component} -e @~/secrets.json &>>/opt/ansible.log
 
