35f133cedb3148b8b070fda50b0df26901f7f89a
Srikanth Palakuri <palakuri.srikanth23@gmail.com>
2024-07-14 21:33:43 +0530

    check

--- main.tf
+++ main.tf
@@ -1,50 +1,50 @@
-# module "frontend" {
-#   depends_on              = [module.backend]
-#   source                  = "./modules/app-asg"
-#   app_port                = 80
-#   bastion_nodes           = var.bastion_nodes
-#   component               = "frontend"
-#   env                     = var.env
-#   instance_type           = var.instance_type
-#   max_capacity            = var.max_capacity
-#   min_capacity            = var.min_capacity
-#   prometheus_nodes        = var.prometheus_nodes
-#   server_app_port_sg_cidr = var.public_subnets
-#   subnets                 = module.vpc.frontend_subnets
-#   vpc_id                  = module.vpc.vpc_id
-#   vault_token             = var.vault_token
-#   certificate_arn         = var.certificate_arn
-#   lb_app_port_sg_cidr     = ["0.0.0.0/0"]
-#   lb_ports                = { http : 80, https : 443 }
-#   lb_subnets              = module.vpc.public_subnets
-#   lb_type                 = "public"
-#   zone_id                 = var.zone_id
-#   kms_key_id              = var.kms_key_id
-# }
+module "frontend" {
+  depends_on              = [module.backend]
+  source                  = "./modules/app-asg"
+  app_port                = 80
+  bastion_nodes           = var.bastion_nodes
+  component               = "frontend"
+  env                     = var.env
+  instance_type           = var.instance_type
+  max_capacity            = var.max_capacity
+  min_capacity            = var.min_capacity
+  prometheus_nodes        = var.prometheus_nodes
+  server_app_port_sg_cidr = var.public_subnets
+  subnets                 = module.vpc.frontend_subnets
+  vpc_id                  = module.vpc.vpc_id
+  vault_token             = var.vault_token
+  certificate_arn         = var.certificate_arn
+  lb_app_port_sg_cidr     = ["0.0.0.0/0"]
+  lb_ports                = { http : 80, https : 443 }
+  lb_subnets              = module.vpc.public_subnets
+  lb_type                 = "public"
+  zone_id                 = var.zone_id
+  kms_key_id              = var.kms_key_id
+}
 
-# module "backend" {
-#   depends_on              = [module.rds]
-#   source                  = "./modules/app-asg"
-#   app_port                = 8080
-#   bastion_nodes           = var.bastion_nodes
-#   component               = "backend"
-#   env                     = var.env
-#   instance_type           = var.instance_type
-#   max_capacity            = var.max_capacity
-#   min_capacity            = var.min_capacity
-#   prometheus_nodes        = var.prometheus_nodes
-#   server_app_port_sg_cidr = concat(var.frontend_subnets, var.backend_subnets)
-#   subnets                 = module.vpc.backend_subnets
-#   vpc_id                  = module.vpc.vpc_id
-#   vault_token             = var.vault_token
-#   certificate_arn         = var.certificate_arn
-#   lb_app_port_sg_cidr     = var.frontend_subnets
-#   lb_ports                = { http : 8080 }
-#   lb_subnets              = module.vpc.backend_subnets
-#   lb_type                 = "private"
-#   zone_id                 = var.zone_id
-#   kms_key_id              = var.kms_key_id
-# }
+module "backend" {
+  depends_on              = [module.rds]
+  source                  = "./modules/app-asg"
+  app_port                = 8080
+  bastion_nodes           = var.bastion_nodes
+  component               = "backend"
+  env                     = var.env
+  instance_type           = var.instance_type
+  max_capacity            = var.max_capacity
+  min_capacity            = var.min_capacity
+  prometheus_nodes        = var.prometheus_nodes
+  server_app_port_sg_cidr = concat(var.frontend_subnets, var.backend_subnets)
+  subnets                 = module.vpc.backend_subnets
+  vpc_id                  = module.vpc.vpc_id
+  vault_token             = var.vault_token
+  certificate_arn         = var.certificate_arn
+  lb_app_port_sg_cidr     = var.frontend_subnets
+  lb_ports                = { http : 8080 }
+  lb_subnets              = module.vpc.backend_subnets
+  lb_type                 = "private"
+  zone_id                 = var.zone_id
+  kms_key_id              = var.kms_key_id
+}
 
 module "rds" {
   source = "./modules/rds"
@@ -78,10 +78,10 @@ module "vpc" {
   availability_zones     = var.availability_zones
 }
 
-module "eks" {
-  source     = "./modules/eks"
-  env        = var.env
-  subnet_ids = module.vpc.backend_subnets
-  kms_key_id = var.kms_key_id
-}
+# module "eks" {
+#   source     = "./modules/eks"
+#   env        = var.env
+#   subnet_ids = module.vpc.backend_subnets
+#   kms_key_id = var.kms_key_id
+# }
 
